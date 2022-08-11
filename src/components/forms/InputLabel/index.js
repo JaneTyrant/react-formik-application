a5e0017983eac282fc3e0c3f8f4b953b8cf94a45
TyrantJane <nonstop00066@gmail.com>
2022-08-11 15:50:40 +0300

    Add InputLabel

---
+++ src/components/forms/InputLabel/index.js
@@ -0,0 +1,23 @@
+import { ErrorMessage, Field } from "formik";
+import React from "react";
+import cx from "classnames";
+import styles from "./InputLabel.module.scss";
+
+const InputLabel = (props) => {
+  const { name, ...restProps } = props;
+  return (
+    <label>
+    <Field name={name}>
+      {({ field, form, meta }) => {
+        const inputClasses = cx(styles.input,  {
+          [styles.invalid] : meta.error && meta.touched
+        })
+        return <input {...field} className={inputClasses} {...restProps}/>;
+      }}
+    </Field>
+    <ErrorMessage name={name} component="div" className={styles.error} />
+  </label>
+  );
+};
+
+export default InputLabel;
--- src/components/forms/LoginForm/index.js
+++ src/components/forms/LoginForm/index.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import { Form, Formik } from "formik";
+import React from "react";
+import { SCHEMA_LOGIN } from "../../../utils/validateSchemas";
+import InputLabel from "../InputLabel";
 
-const LoginForm = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-}
+const initialValues = {
+  login: "",
+};
+
+const LoginForm = (props) => {
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={props.onSubmit}
+      validationSchema={SCHEMA_LOGIN}
+    >
+      {(formikProps) => {
+        return (
+          <Form>
+            <InputLabel name="login" type="text" placeholder="login" />
+          </Form>
+        );
+      }}
+    </Formik>
+  );
+};
 
 export default LoginForm;

a35f4a93dd5010f86485d8d0f0d1f679aea67078
TyrantJane <nonstop00066@gmail.com>
2022-08-11 15:07:58 +0300

    Add header structure

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
 
 const Header = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-}
+  const { pathname } = useLocation();
+  return (
+    <header>
+      <Link to="/">
+        <img src="./static/images/logo.png" alt="logo" />
+      </Link>
+      {pathname === "/login" ? (
+        <Link to="/signup">Signup</Link>
+      ) : (
+        <Link to="/login">Login</Link>
+      )}
+      <h1>home</h1>
+    </header>
+  );
+};
 
 export default Header;

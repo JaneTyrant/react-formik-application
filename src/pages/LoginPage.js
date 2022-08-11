4c4584a95ebc59d449becb158783e4cea806fad5
TyrantJane <nonstop00066@gmail.com>
2022-08-11 15:17:12 +0300

    Add LoginPage and Signup page structure

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
+import LoginForm from "../components/forms/LoginForm";
 
 const LoginPage = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-}
+  return (
+    <section>
+      <h1>login to your account</h1>
+      <LoginForm />
+    </section>
+  );
+};
 
 export default LoginPage;
--- src/pages/SignupPage.js
+++ src/pages/SignupPage.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
+import SignupForm from "../components/forms/SignupForm";
 
 const SignupPage = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-}
+  return (
+    <section>
+      <h1>create an account</h1>
+      <h2>We always keep your name and email adress private.</h2>
+      <SignupForm />
+    </section>
+  );
+};
 
 export default SignupPage;
